89c64c16ffdeeebef09bc8e7bdbae9d509e2c676
Bui Nguyen Truc <buinguyentruc@luvina.net>
2024-12-31 17:51:19 +0700

    1 tieng 40 m

--- controllers/client/home_controller.js
+++ controllers/client/home_controller.js
@@ -1,3 +1,7 @@
+const ProductCategory = require("../../models/product_category_model")
+const { tree } = require("../../helper/create_tree.js");
 module.exports.index = async (req, res) => {
-  res.render("client/pages/home/index", { pageTitle: "Trang chủ" });
+  const productCategory = await ProductCategory.find({deleted:false})
+  const newProductCategory = tree(productCategory)
+  res.render("client/pages/home/index", { pageTitle: "Trang chủ",layoutProductCategory:newProductCategory });
 };
